be6715739dd685b36692f7ad50392cadc0603560
dowonseo <dsa3182@gmail.com>
2024-03-18 16:28:22 +0900

    Add layout structure

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="bg-gray-200">
+            <h1>헤더입니다</h1>
+        </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
+import Nav from "../components/layout/Nav";
+
 export default function Home() {
     return (
-        <div>
+        <>
+            <Header />
+            <Nav />
             <h1>Hello, React!</h1>
-        </div>
+            <Footer />
+        </>
     );
 }
